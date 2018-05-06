439a1059a466be9273feff449b03e92016d43ef9
Alicewillbe <zhaolonglong4008@gmail.com>
2018-05-06 01:55:57 -0700

    finish ReflectiveCallable.java

--- src/main/java/org/junit/internal/runners/model/ReflectiveCallable.java
+++ src/main/java/org/junit/internal/runners/model/ReflectiveCallable.java
@@ -8,11 +8,15 @@ import java.lang.reflect.InvocationTargetException;
  * When invoked, throws the exception from the reflected method, rather than
  * wrapping it in an InvocationTargetException.
  */
+@SuppressWarnings("nullness")
 public abstract class ReflectiveCallable {
     public @Nullable Object run() throws Throwable {
         try {
             return runReflectiveCall();
         } catch (InvocationTargetException e) {
+            // [throwing.nullable] TRUE_POSITIVE
+            // the getTargetException has same behavior as getCause,
+            // which is documented that can return null
             throw e.getTargetException();
         }
     }
