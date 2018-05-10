6b85f3221d935a191272f83b63aa27aac457c3a4
Anny(Yanmeng Kong) <yk57@cs.washington.edu>
2018-05-10 02:05:49 -0700

    yk - FrameworkMember

--- src/main/java/org/junit/runners/model/FrameworkMember.java
+++ src/main/java/org/junit/runners/model/FrameworkMember.java
@@ -1,5 +1,7 @@
 package org.junit.runners.model;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
+
 import java.lang.reflect.Modifier;
 import java.util.List;
 
@@ -18,7 +20,7 @@ public abstract class FrameworkMember<T extends FrameworkMember<T>> implements
      * 
      * @return member that should be used, or {@code null} if no member should be used.
      */
-    final T handlePossibleShadowedMember(List<T> members) {
+    final @Nullable T handlePossibleShadowedMember(List<T> members) {
         for (int i = members.size() - 1; i >=0; i--) {
             T otherMember = members.get(i);
             if (isShadowedBy(otherMember)) {
